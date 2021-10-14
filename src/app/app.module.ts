f78aeee4f5cf95d1dbcbe193fdbe47552aca2948
Bhavya30823 <60810721+Bhavya30823@users.noreply.github.com>
2021-10-14 13:54:48 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
