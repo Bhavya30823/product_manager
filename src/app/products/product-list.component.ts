69ab56d15329b40fdd831cffcf55c97e189a58dc
Bhavya30823 <60810721+Bhavya30823@users.noreply.github.com>
2021-10-14 19:08:12 +0530

    filter box added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Iproduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string ="Product List!";
     imageWidth: number = 50;
     imageMargin: number= 2; 
     showImage: boolean = false;
-    listFilter: string = "cart";
-    products: any[]=[
+    private _listFilter: string = " ";
+    
+    get listFilter(): string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter= value;
+        this.filteredProducts= this.performFilter(value);
+    }
+    filteredProducts: Iproduct[]=[];
+    products: Iproduct[]=[
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -33,8 +44,16 @@ export class ProductListComponent{
           },
     ];
 
+    performFilter(filterBy: string):Iproduct[] {
+        filterBy= filterBy.toLowerCase();
+        return this.products.filter((product : Iproduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
     toggleImage(): void{
         this.showImage= !this.showImage; 
+    }
+    ngOnInit(): void{
+        console.log('ON IT');
     }
 }
 
